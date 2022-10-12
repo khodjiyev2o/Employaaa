ae6602d3e0209deb6c1cb86772045cf5887cb44c
samandar <samandar@samandarui-MacBookAir.local>
2022-10-13 01:18:54 +0900

    completed

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from fastapi import FastAPI
+import databases
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+
+DATABASE_URL = "sqlite:///./main.db"
+db = databases.Database(DATABASE_URL) 
+  
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
 
 
 @app.get("/")
