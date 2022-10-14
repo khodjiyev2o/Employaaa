f8079d86191be6806968111edef0d02909abfb43
samandar <samandar@samandarui-MacBookAir.local>
2022-10-14 14:38:11 +0900

    env file and sample.env file

--- database.py
+++ database.py
@@ -6,8 +6,14 @@ import os
 load_dotenv()
 
 metadata = sqlalchemy.MetaData()
-DATABASE_URL=f'postgresql://postgres:postgres@postgres:5432/postgres'
+DATABASE_URL='postgresql://postgres:postgres@postgres:5432/postgres'
 
+#did not work
+#DATABASE_URL = os.environ.get("DATABASE_URL")
+#DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+print(DATABASE_URL)
 db = databases.Database(DATABASE_URL)
 
 engine = create_engine(
