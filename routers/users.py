9ca8c3e92891ccd032a07b8c2b1031d1dd58291c
samandar <samandar@samandarui-MacBookAir.local>
2022-10-17 22:47:37 +0900

    crud function are now in Crud class

--- routers/users.py
+++ routers/users.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from users import database,schemas,crud ,models
 
 from sqlalchemy.orm import Session
-
+from users.crud import Crud
 router = APIRouter()
 
 router = APIRouter(
@@ -21,31 +21,36 @@ get_db = database.get_db
 
 
 
-
 @router.post('/create_users',response_model=schemas.User)
 def create_user(request: schemas.UserSignUp,db: Session = Depends(get_db))->schemas.User:
-    return crud.create_user(request,db)
+    crud = Crud(db=db)
+    return crud.create_user(request)
 
 @router.put('/{id}',response_model=schemas.User,status_code=status.HTTP_202_ACCEPTED)
 def update(id:int, request: schemas.UserUpdate, db: Session = Depends(get_db))->schemas.User:
-    return crud.update(id=id,request=request, db=db)
+    crud = Crud(db=db)
+    return crud.update(id=id,request=request)
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id:int, db: Session = Depends(get_db)):
-    return crud.destroy(db=db,id=id)
+    crud = Crud(db=db)
+    return crud.destroy(id=id)
 
 
 @router.get('/email/{email}',response_model=schemas.User)
 def get_user_by_email(email:str,db: Session = Depends(get_db))->schemas.User:
-    return crud.get_user_by_email(db=db,email=email) 
+    crud = Crud(db=db)
+    return crud.get_user_by_email(email=email) 
 
 
 @router.get('/{id}',response_model=schemas.User)
 def get_user(id:int,db: Session = Depends(get_db))->schemas.User:
-    return crud.get_user_by_id(id,db)   
+    crud = Crud(db=db)
+    return crud.get_user_by_id(id)   
 
 
 
 @router.get('/')
 def all(db: Session = Depends(get_db))->schemas.User:
-    return crud.get_users(db)
+    crud = Crud(db=db)
+    return crud.get_users()
